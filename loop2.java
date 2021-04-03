5d26e6ae4f448b3ef4e16642f4fe5474911d79f6
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-04-03 16:36:50 +0700

    Add files via upload

---
+++ loop2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class loop2
+{
+  public static void main(String[] args)
+  {
+    Scanner kb = new Scanner(System.in);
+    int x = kb.nextInt();
+    for(int i =1; i<=x; i++)
+    {
+      System.out.println("java");
+    }
+  }
+}
