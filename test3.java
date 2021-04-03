ce376fb7b6402cb7056567f9c029f00e2584abd7
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-04-03 16:27:51 +0700

    Add files via upload

---
+++ test3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class test3
+{
+  public static void main(String[] args)
+  {
+    Scanner kb = new Scanner(System.in);
+    float x = kb.nextFloat();
+    double pi  = 3.14;
+    double y = pi*(x*x);
+    System.out.println(y);
+  }
+}
